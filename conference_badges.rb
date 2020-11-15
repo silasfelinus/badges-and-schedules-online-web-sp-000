f101dfe6b4d40f657d195f61ef708f36eea0c7f3
Silas Michael Knight <silasfelinus@gmail.com>
2020-11-15 00:07:12 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,24 @@
 # Write your code here.
+def badge_maker(name)
+  "Hello, my name is #{name}."
+end
+
+def assign_rooms(attendees)
+  rooms = []
+  attendees.each_with_index do |name, index|
+    rooms << "Hello, #{name}! You'll be assigned to room #{index+1}!"
+  end
+  rooms
+end
+
+def batch_badge_creator(attendees)
+  badges = []
+  attendees.each do |name|
+    badges << badge_maker(name)
+  end
+  badges
+end
+
+def printer()
+
+end
